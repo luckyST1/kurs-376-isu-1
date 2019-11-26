586ccf30a678fc74f126d1e00223e3da437d49de
Никита <n1cetime@yandex.ru>
2019-11-26 23:00:51 +0300

    llo

--- views/site/correct.php
+++ views/site/correct.php
@@ -20,7 +20,7 @@ $form = ActiveForm::begin();
 $otvet = \app\models\TestCreate::find('question')->all();
 //$otvet1 = \app\models\TestCreate::find()->all();
 //$otvet2 = \app\models\TestCreate::find()->all();
-//$otvet3 = \app\models\TestCreate::find()->all();
+
 // формируем массив, с ключем равным полю 'id' и значением равным полю 'йгуыешщт'
 $items = ArrayHelper::map($otvet, 'question', function($item1) { return $item1->radio1 ; function($item2)  {return $item2->radio2;};});
 //$item1 =
